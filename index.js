713c409ac4e4b54a85cee8150e5a9b5892a3eeb5
yxiong-arthur <arthur_world@outlook.com>
2023-01-25 13:11:55 -0800

    My third commit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 
-mongoose.connect('mongodb://localhost/MovieRentalApp')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
-
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+
+mongoose.connect('mongodb://localhost/MovieRentalApp')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
