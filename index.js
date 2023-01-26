a8490bf20f036c42fc687d57496f3d3bbdabaa87
yxiong-arthur <arthur_world@outlook.com>
2023-01-26 14:55:58 -0800

    finish movie feature

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
+const movies = require('./routes/movies');
 
 mongoose.connect('mongodb://localhost/MovieRentalApp')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port} ...`));
--- models/genre.js
+++ models/genre.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 50
     }
-}));
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
     const schema = Joi.object({ name: Joi.string().min(1).required() });
     return schema.validate(genre);
 }
 
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validateGenre;
